e9bfe637d19141da57284f745b569d28efb99a87
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-19 16:43:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'functions/password_generator.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'components/head/head.php'; ?>
+</head>
+
+<body>
+    <?php include 'components/body/header.php'; ?>
+    <?php include 'components/body/main.php'; ?>
+    <?php include 'components/body/footer.php'; ?>
+</body>
+
+</html>
